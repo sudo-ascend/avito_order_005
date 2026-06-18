1ed956b6d79e3722d0e311628920bb983f8f2000
sudo-ascend <grachevilia09@yandex.ru>
2026-06-18 20:52:30 +0300

    web master favicon + 404 page commit

--- catalog/views.py
+++ catalog/views.py
@@ -452,3 +452,19 @@ def yandex_verification(request):
         "catalog/yandex_4d153c26552f0309.html",
         content_type="text/html; charset=utf-8",
     )
+
+
+def bad_request(request, exception):
+    return render(request, "400.html", status=400)
+
+
+def permission_denied(request, exception):
+    return render(request, "403.html", status=403)
+
+
+def page_not_found(request, exception):
+    return render(request, "404.html", status=404)
+
+
+def server_error(request):
+    return render(request, "500.html", status=500)
--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,21 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("favicon.ico", RedirectView.as_view(url=settings.STATIC_URL + "favicon.ico", permanent=True)),
     path('', include('catalog.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler400 = "catalog.views.bad_request"
+handler403 = "catalog.views.permission_denied"
+handler404 = "catalog.views.page_not_found"
+handler500 = "catalog.views.server_error"
